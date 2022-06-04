439afa9b6b9c6ef713d0f999d7fd162c31f140db
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-06-04 17:41:17 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// Clase 13 - Lunes 30 de mayo de 2022
+//Ejercicios de repaso (no obligatorios)
+
+//Ejercicio 1:
+
+/*Una materia de la carrera de Ingeniería de Sistemas exige 
+la entrega de un trabajo práctico cada mes del año académico. 
+Este arreglo contiene las notas de un alumno 
+para los meses de marzo a diciembre:
+
+notasAlumno: [7, 6, 9, 5, 8, 7, 10, 4, 8, 8]
+
+1) Teniendo en cuenta que se aprueba con 6, crear una 
+función que devuelva en que meses desaprobó el alumno.
+2) Crear una función que devuelva en que mes obtuvo 
+menor nota el alumno y cuál fue la nota.
+3) Crear una función que devuelva en que mes obtuvo 
+mayor nota el alumno y cuál fue la nota.
+4) Crear una función que devuelva el promedio del alumno.
+*/
