c5612d4dfe8f9b9bfee8d11a6fd66d1aca859f13
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-06-05 00:09:49 -0300

    clase13-repaso correcciones en el ejercicio 1 no obligatorio

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,79 @@ menor nota el alumno y cuál fue la nota.
 mayor nota el alumno y cuál fue la nota.
 4) Crear una función que devuelva el promedio del alumno.
 */
+
+let notasAlumno: number[] = [7, 6, 9, 5, 8, 7, 10, 4, 8, 8];
+let mesesNotas: string[] = [
+  "Marzo - ",
+  "Abril - ",
+  "Mayo - ",
+  "Junio - ",
+  "Julio - ",
+  "Agosto - ",
+  "Septiembre - ",
+  "Octubre - ",
+  "Noviembre - ",
+  "Diciembre - "
+];
+let indice: number;
+
+// 1) Función que devuelve en qué meses desaprobó el alumno.
+function desaproboMeses(notas: number[], mes: string[]): string {
+  let mesesDesaprobados: string = "";
+  for (indice = 0; indice < notas.length; indice++) {
+    if (notas[indice] < 6) {
+      mesesDesaprobados += mes[indice] + "";
+    }
+  }
+
+  return mesesDesaprobados;
+}
+
+// 2) Función que devuelve en qué mes obtuvo menor nota el alumno y cuál fue la nota.
+function menorNota(notas: number[], mes: string[]): string {
+  let menor: number = 10;
+  let mesMenor: string = "";
+  let texto: string = "";
+  for (indice = 0; indice < notas.length; indice++) {
+    if (notas[indice] < menor) {
+      menor = notas[indice];
+      mesMenor = mes[indice];
+    }
+  }
+  texto = "mes de " + mesMenor + "Nota: " + menor;
+  return texto;
+}
+
+// 3) Función que devuelve en qué mes obtuvo mayor nota el alumno y cuál fue la nota.
+function mayorNota(notas: number[], mes: string[]): string {
+  let mayor: number = 0;
+  let mesMayor: string = "";
+  let texto: string = "";
+  for (indice = 0; indice < notas.length; indice++) {
+    if (notas[indice] > mayor) {
+      mayor = notas[indice];
+      mesMayor = mes[indice];
+    }
+  }
+  texto = "mes de " + mesMayor + "Nota: " + mayor;
+  return texto;
+}
+
+// 4) Función que devuelve el promedio del alumno.
+function promedioNota(notas: number[]): number {
+  let suma: number = 0;
+  let promedio: number = notas.length;
+  for (indice = 0; indice < notas.length; indice++) suma += notas[indice];
+  return suma / promedio;
+}
+
+console.log(
+  "Los meses desaprobados son: " + desaproboMeses(notasAlumno, mesesNotas)
+);
+console.log(
+  "El mes que obtuvo menor nota es: " + menorNota(notasAlumno, mesesNotas)
+);
+console.log(
+  "El mes que obtuvo mayor nota es: " + mayorNota(notasAlumno, mesesNotas)
+);
+console.log("El promedio del alumno es: " + promedioNota(notasAlumno));
